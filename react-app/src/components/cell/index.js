bd5631c1011f0f3a3d2cc3fccae4e9ed8f315390
Benjamin Rose <benthere914@gmail.com>
2022-01-06 17:02:48 -0600

    basic tictactoe

--- react-app/src/components/cell/index.js
+++ react-app/src/components/cell/index.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react'
 import './index.css'
-const Cell = ({value}) => {
+const Cell = ({x, y, values, setValues, myChar, setYourTurn, yourTurn}) => {
+    const cellClickHandler = () => {
+        if (!yourTurn) return
+        setValues((prev) => {
+            prev[`${x},${y}`] = myChar;
+            return prev
+        })
+        setYourTurn((prev) => !prev)
+    }
     return (
         <>
-            <div className='cell'>
-                {value}
+            <div className='cell' onClick={cellClickHandler}>
+                <p className='cellValue'>{values[`${x},${y}`]}</p>
             </div>
         </>
     )
--- react-app/src/components/home/index.js
+++ react-app/src/components/home/index.js
@@ -1,17 +1,35 @@
-import './index.css'
+import './index.css';
 import Cell from '../cell';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 const Home = () => {
     const rows = [0, 1, 2];
     const cols = [0, 1, 2];
+    const [yourTurn, setYourTurn] = useState(true);
+    const [myChar, setMyChar] = useState('X')
+    const [values, setValues] = useState({
+        '0,0': '',
+        '0,1': '',
+        '0,2': '',
+        '1,0': '',
+        '1,1': '',
+        '1,2': '',
+        '2,0': '',
+        '2,1': '',
+        '2,2': '',
+    })
+
+    console.log(values)
     return (
         <>
             <div className='homePage'>
                 <h1>Live Tic-Tac-Toe</h1>
+
                 {rows.map((x) => (
-                    <div className='rows'>
+                    <div className='rows' key={uuidv4()}>
                         {cols.map((y) => (
-                            <div className='cols'>
-                                <Cell value={`${x}, ${y}`}/>
+                            <div className='cols' key={uuidv4()}>
+                                <Cell x={x} y={y} myChar={myChar} setValues={setValues} values={values} setYourTurn={setYourTurn} yourTurn={yourTurn}/>
                             </div>
                         ))}
                     </div>
