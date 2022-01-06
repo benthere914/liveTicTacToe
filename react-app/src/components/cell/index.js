be313a5bd7dc0c5cff2c953d8a296c6d89c71a27
Benjamin Rose <benthere914@gmail.com>
2022-01-06 16:19:34 -0600

    made basic tictactoe board

---
+++ react-app/src/components/cell/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const Cell = ({value}) => {
+    return (
+        <>
+            <div className='cell'>
+                {value}
+            </div>
+        </>
+    )
+}
+
+export default Cell
--- react-app/src/components/home/index.js
+++ react-app/src/components/home/index.js
@@ -1,9 +1,21 @@
 import './index.css'
+import Cell from '../cell';
 const Home = () => {
+    const rows = [0, 1, 2];
+    const cols = [0, 1, 2];
     return (
         <>
             <div className='homePage'>
-                <h1>The site is loaded. You can now edit the home page or add other components</h1>
+                <h1>Live Tic-Tac-Toe</h1>
+                {rows.map((x) => (
+                    <div className='rows'>
+                        {cols.map((y) => (
+                            <div className='cols'>
+                                <Cell value={`${x}, ${y}`}/>
+                            </div>
+                        ))}
+                    </div>
+                ))}
             </div>
         </>
     )
