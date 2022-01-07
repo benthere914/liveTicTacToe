5d643451c527ac5caaae211110ff61c5df4eba29
Benjamin Rose <benthere914@gmail.com>
2022-01-06 19:18:20 -0600

    basic websockets works - still has some bugs

--- app/socketIO.py
+++ app/socketIO.py
@@ -13,5 +13,23 @@ else:
 # create your SocketIO instance
 socketio = SocketIO(cors_allowed_origins=origins)
 
+rooms = [[]]
+@socketio.on("setUp")
+def send_chat_out():
+    num_of_rooms = len(rooms)
+    in_last_room = len(rooms[num_of_rooms - 1])
+    if (in_last_room == 2):
+        rooms.append(['O'])
+    elif (in_last_room == 1):
+        rooms[num_of_rooms - 1].extend('X')
+    else:
+        rooms[num_of_rooms - 1].extend('O')
+    # print(rooms[-1][-1])
+    join_room(num_of_rooms)
+    emit('setUpChar', {'char': rooms[-1][-1], 'room': num_of_rooms})
 
-
+@socketio.on("updateBoard")
+def update_board(data):
+    turns = {'X': 'O', 'O': 'X'}
+    print(turns[data['lastPlayed']])
+    emit("updateBoard", {'values': data['values'], 'turn': turns[data['lastPlayed']]}, broadCast=True, include_self=True, room=data['room'])
--- react-app/src/components/cell/index.js
+++ react-app/src/components/cell/index.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react'
 import './index.css'
-const Cell = ({x, y, values, setValues, myChar, setYourTurn, yourTurn}) => {
+const Cell = ({x, y, values, setValues, myChar, setYourTurn, yourTurn, socket, roomId, nextTurn}) => {
     const cellClickHandler = () => {
-        if (!yourTurn) return
+        // if (nextTurn === myChar) return
         setValues((prev) => {
             prev[`${x},${y}`] = myChar;
+            socket.emit('updateBoard', {'values': prev, 'room': roomId, 'lastPlayed': myChar})
             return prev
         })
-        setYourTurn((prev) => !prev)
+        // setYourTurn(prev => !prev)
     }
+
     return (
         <>
             <div className='cell' onClick={cellClickHandler}>
